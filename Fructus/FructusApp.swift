a5a86288e567c722a31c908f60f35b3ba9b38657
Valentina Bulanova <valyabulanova@gmail.com>
2021-12-19 10:07:33 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Валентина Буланова on 19.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
